8fe58951d4e8d609e2de67026dcdb0bd4b45b00a
Steven Abreu <steven.abreu@intel.com>
2024-05-17 05:24:01 -0700

    script to install dependencies

---
+++ install_deps.sh
@@ -0,0 +1,13 @@
+python -m venv venv
+source venv/bin/activate
+
+pip install -U pip
+pip install -U "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+
+cd aqt
+pip install .
+cd ..
+
+pip install wandb tqdm einops
+pip install torch torchtext torchaudio torchvision
+pip install datasets tensorflow-datasets
