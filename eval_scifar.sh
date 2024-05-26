8b3f67d6a830e247a72ec8a92e12097c5fad7c2a
Steven Abreu <steven.abreu@intel.com>
2024-05-26 05:44:53 -0700

    PTQ scripts & results for text, scifar, listops

---
+++ eval_scifar.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+#SBATCH -c 14
+#SBATCH -t 10:00:00
+#SBATCH -o /home/sabreu/NeuroSSMs/logs/slurm-%j.out
+
+# Default values
+a_bits=None
+ssm_act_bits=None
+non_ssm_act_bits=None
+non_ssm_bits=None
+b_bits=None
+c_bits=None
+d_bits=None
+qgelu_approx="False"
+hard_sigmoid="False"
+batchnorm="True"
+run_name=None
+load_run_name=None
+checkpoint_dir="${HOME}/NeuroSSMs/checkpoints"
+use_qlayernorm_if_quantized="True"
+remove_norm_bias_from_checkpoint="False"
+use_layernorm_bias="True"
+
+# Parse arguments
+for i in "$@"
+do
+case $i in
+    --a_bits=*)
+    a_bits="${i#*=}"
+    shift # past argument=value
+    ;;
+    --ssm_act_bits=*)
+    ssm_act_bits="${i#*=}"
+    shift # past argument=value
+    ;;
+    --non_ssm_act_bits=*)
+    non_ssm_act_bits="${i#*=}"
+    shift # past argument=value
+    ;;
+    --non_ssm_bits=*)
+    non_ssm_bits="${i#*=}"
+    shift # past argument=value
+    ;;
+    --b_bits=*)
+    b_bits="${i#*=}"
+    shift # past argument=value
+    ;;
+    --c_bits=*)
+    c_bits="${i#*=}"
+    shift # past argument=value
+    ;;
+    --d_bits=*)
+    d_bits="${i#*=}"
+    shift # past argument=value
+    ;;
+    --hard_sigmoid)
+    hard_sigmoid="True"
+    shift # past argument with no value
+    ;;
+    --qgelu_approx)
+    qgelu_approx="True"
+    shift # past argument with no value
+    ;;
+    --batchnorm=*)
+    batchnorm="${i#*=}"
+    shift # past argument=value
+    ;;
+    --use_qlayernorm_if_quantized=*)
+    use_qlayernorm_if_quantized="${i#*=}"
+    shift # past argument=value
+    ;;
+    --remove_norm_bias_from_checkpoint=*)
+    remove_norm_bias_from_checkpoint="${i#*=}"
+    shift # past argument=value
+    ;;
+    --use_layernorm_bias=*)
+    use_layernorm_bias="${i#*=}"
+    shift # past argument=value
+    ;;
+    --run_name=*)
+    run_name="${i#*=}"
+    shift # past argument=value
+    ;;
+    --load_run_name=*)
+    load_run_name="${i#*=}"
+    shift # past argument=value
+    ;;
+    --checkpoint_dir=*)
+    checkpoint_dir="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+# Prepare optional parameter strings
+args=""
+for var in remove_norm_bias_from_checkpoint use_layernorm_bias a_bits ssm_act_bits non_ssm_act_bits non_ssm_bits b_bits c_bits d_bits hard_sigmoid qgelu_approx use_qlayernorm_if_quantized batchnorm load_run_name run_name checkpoint_dir; do
+    value=$(eval echo \$$var)
+    if [ "$value" != "None" ]; then
+        args+=" --$var=$value"
+    fi
+done
+
+echo "Running with args: $args"
+
+# setup pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+###############################################################
+
+export WANDB_APIKEY="$(cat wandb_apikey.txt)"
+
+# activate venv
+source ./venv/bin/activate
+
+# enter the right directory
+cd /home/sabreu/NeuroSSMs/S5fork
+
+# run the script
+# d_model           H: dims for input/output features
+# ssm_size_base     P: latent size in SSM
+# blocks            J: number of blocks used to initialize A
+# NOTE: batchnorm is included in the $args variable! specify this from `sbatch run_smnist.sh --batchnorm={True|False}`
+python run_qeval.py \
+    --USE_WANDB=TRUE --wandb_project=NeuroSSMs --wandb_entity=il-ncl \
+    $args \
+    --dataset=lra-cifar-classification \
+    --n_layers=6 --d_model=512 --ssm_size_base=384 --blocks=3 \
+    --prenorm=True --bsz=50 --epochs=250 \
+    --ssm_lr_base=0.001 --lr_factor=4.5 --p_dropout=0.1 --weight_decay=0.07 \
+    --jax_seed=16416 \
+    --C_init=lecun_normal --bidirectional=True --clip_eigs=True \
+    --opt_config=BfastandCdecay \
+    --warmup_end=1
+    # --dataset=mnist-classification \
+    # --n_layers=4 --d_model=96 --ssm_size_base=128 --blocks=1 \
+    # --prenorm=TRUE --bidirectional=FALSE \
+    # --ssm_lr_base=0.004 --lr_factor=4 --p_dropout=0.1 --weight_decay=0.01 \
+    # --bsz=50 --epochs=150
---
+++ run_ptq_scifar_eval_lnnb.sh
@@ -0,0 +1,53 @@
+echo "FP: eval-ptq--scifar-full-ln_nb--fp"
+sbatch eval_scifar.sh \
+    --load_run_name=scifar-full-ln_nb --batchnorm=False \
+    --use_layernorm_bias=False \
+    --run_name=eval-ptq--scifar-full-ln_nb--fp
+
+echo "W8A8"
+echo "W8A8 gelu, sigmoid, LayerNorm: eval-ptq--scifar-full-ln_nb--W8A8-gelu-sigmoid-ln"
+sbatch eval_scifar.sh \
+    --load_run_name=scifar-full-ln_nb --batchnorm=False \
+    --non_ssm_act_bits=8 --ssm_act_bits=8 \
+    --a_bits=8 --b_bits=8 --c_bits=8 --d_bits=8 --non_ssm_bits=8 \
+    --use_qlayernorm_if_quantized=False --use_layernorm_bias=False \
+    --run_name=eval-ptq--scifar-full-ln_nb--W8A8-gelu-sigmoid-ln
+
+echo "W8A8 + qgelu"
+echo "W8A8 qgelu, sigmoid, LayerNorm: eval-ptq--scifar-full-ln_nb--W8A8-qgelu-sigmoid-ln"
+sbatch eval_scifar.sh \
+    --load_run_name=scifar-full-ln_nb --batchnorm=False \
+    --non_ssm_act_bits=8 --ssm_act_bits=8 \
+    --a_bits=8 --b_bits=8 --c_bits=8 --d_bits=8 --non_ssm_bits=8 \
+    --use_qlayernorm_if_quantized=False --use_layernorm_bias=False \
+    --qgelu_approx \
+    --run_name=eval-ptq--scifar-full-ln_nb--W8A8-qgelu-sigmoid-ln
+
+echo "W8A8 + hard sigmoid"
+echo "W8A8 gelu, hard sigmoid, LayerNorm: eval-ptq--scifar-full-ln_nb--W8A8-gelu-hsigmoid-ln"
+sbatch eval_scifar.sh \
+    --load_run_name=scifar-full-ln_nb --batchnorm=False \
+    --non_ssm_act_bits=8 --ssm_act_bits=8 \
+    --a_bits=8 --b_bits=8 --c_bits=8 --d_bits=8 --non_ssm_bits=8 \
+    --use_qlayernorm_if_quantized=False --use_layernorm_bias=False \
+    --hard_sigmoid \
+    --run_name=eval-ptq--scifar-full-ln_nb--W8A8-gelu-hsigmoid-ln
+
+echo "W8A8 + qlayernorm"
+echo "W8A8 Gelu, Sigmoid, qLayerNorm: eval-ptq--scifar-full-ln_nb--W8A8-gelu-sigmoid-qln"
+sbatch eval_scifar.sh \
+    --load_run_name=scifar-full-ln_nb --batchnorm=False \
+    --non_ssm_act_bits=8 --ssm_act_bits=8 \
+    --a_bits=8 --b_bits=8 --c_bits=8 --d_bits=8 --non_ssm_bits=8 \
+    --use_qlayernorm_if_quantized=True --use_layernorm_bias=True \
+    --run_name=eval-ptq--scifar-full-ln_nb--W8A8-gelu-sigmoid-qln
+
+echo "W8A8 + qgelu + hard sigmoid + qlayernorm"
+echo "W8A8 Gelu, Sigmoid, qLayerNorm: eval-ptq--scifar-full-ln_nb--W8A8-qgelu-hsigmoid-qln"
+sbatch eval_scifar.sh \
+    --load_run_name=scifar-full-ln_nb --batchnorm=False \
+    --non_ssm_act_bits=8 --ssm_act_bits=8 \
+    --a_bits=8 --b_bits=8 --c_bits=8 --d_bits=8 --non_ssm_bits=8 \
+    --use_qlayernorm_if_quantized=True --use_layernorm_bias=True \
+    --hard_sigmoid --qgelu_approx \
+    --run_name=eval-ptq--scifar-full-ln_nb--W8A8-qgelu-hsigmoid-qln
